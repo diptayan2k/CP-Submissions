789506bfa49a90d77c915e6ce7692754ac549c64
Diptayan Biswas <diptayan2k@gmail.com>
2018-07-05 23:52:00 +0530

    Add solution for problem `B - Sonya and Exhibition` Link: https://codeforces.com/contest/1004/problem/B Tags: constructive algorithms, greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/1004/submission/40005073

---
+++ codeforces/1004/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ long long int i,n,m,l,r;
+cin>>n>>m;
+for(i=0;i<m;i++) cin>>l>>r;
+for(i=0;i<n;i++)
+{ if(i%2==0) cout<<"1";
+   else cout<<"0";
+
+}
+
+
+
+
+
+
+
+    return 0;
+}
