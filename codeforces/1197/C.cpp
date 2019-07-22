329c14d18d8b1a99bdc065572788cd605f3e354f
Diptayan Biswas <diptayan2k@gmail.com>
2019-07-22 22:16:00 +0530

    Add solution for problem `C - Array Splitting` Link: https://codeforces.com/contest/1197/problem/C Tags: greedy, sortings, *1400 Ref: https://codeforces.com/contest/1197/submission/57548613

---
+++ codeforces/1197/C.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define f(i,a,b) for(ll i=a;i<=b;i++)
+#define g(i,b,a) for(ll i=a;i>=b;i--)
+#define F first
+#define vv vector
+#define S second
+#define mp make_pair
+#define pb push_back
+using namespace std;
+
+
+/*
+
+
+void dfs(vv<ll> v[],bool vis[],ll i,ll sum[],ll a[], ll c[])
+{
+      vis[i]=true;
+     sum[i]=a[i];
+     c[i]=0;
+
+    if(!v[i].empty())
+    f(j,0,v[i].size()-1)
+    if(!vis[v[i][j]])
+    {
+    dfs(v,vis,v[i][j],sum,a,c);
+    sum[i]+=sum[v[i][j]];
+    c[i]++;
+    }
+
+
+
+}
+ll p;
+void dfs1(vv<ll> v[],bool vis[],ll i,ll sum[],ll c[], ll a[])
+{
+      vis[i]=true;
+
+
+    if(!v[i].empty())
+    f(j,0,v[i].size()-1)
+    if(!vis[v[i][j]])
+    {
+    dfs1(v,vis,v[i][j],sum,c,a);
+    s=max(s,s-sum[v[i][j]]-m);
+    }
+    p=p-sum[i]-m;
+    s=max(s,p);
+
+
+
+
+}
+
+ll binarySearchCount(ll arr[], ll n, ll key, ll d)
+{ ll left =d;
+ll     right = n;
+
+    ll mid;
+    while (left < right)
+    {
+        mid = (right + left) >> 1;
+
+        // Check if key is present in array
+        if (arr[mid] == key)
+        {
+            // If duplicates are present it returns
+            // the position of last element
+            while (mid+1<n && arr[mid+1] == key)
+                 mid++;
+            break;
+        }
+
+        // If key is smaller, ignore right half
+        else if (arr[mid] > key)
+            right = mid;
+
+        // If key is greater, ignore left half
+        else
+            left = mid + 1;
+    }
+
+    // If key is not found in array then it will be
+    // before mid
+    while (mid > -1 && arr[mid] > key)
+        mid--;
+
+    //Return mid + 1 because of 0-based indexing
+    // of array
+    return mid-d + 1;
+}
+
+*/
+bool sortinrev(const pair<ll,ll> &a,
+               const pair<ll,ll> &b)
+{
+       return (a.first > b.first);
+}
+ll func(string a, string b)
+{ string c;
+    if(a.length()>b.length())
+     { c+=a.substr(0,a.length()-b.length());
+    f(i,0,b.length()-1)
+    {
+      c+=a[a.length()-b.length()+i];
+      c+=b[i];
+    }}
+    if(a.length()==b.length())
+     { //c+=a.substr(0,a.length()-b.length());
+    f(i,0,b.length()-1)
+    {
+      c+=a[i];
+      c+=b[i];
+    }}
+
+    if(a.length()<b.length())
+     { c+=b.substr(0,b.length()-a.length());
+    f(i,0,a.length()-1)
+    {
+
+    c+=a[i];
+    c+=b[b.length()-a.length()+i];
+    }}
+    ll d=0;
+    f(i,0,c.length()-1)
+    {d=((d*10)%998244353+((ll)c[i]-48)%998244353)%998244353;
+
+    }
+    //cout<<c<<endl<<endl;
+    return d;
+}
+
+
+
+int main(){
+    ll n,k;
+    	cin>>n>>k;
+    ll a[n+1];
+    ll diff[n];
+	for(ll i=1;i<=n;i++) cin>>a[i];
+	ll sum=0;
+	for(ll i=1;i<n;i++){
+		diff[i]=a[i+1]-a[i];
+		sum+=diff[i];
+	}
+	sort(diff+1,diff+n);
+	for(ll i=1;i<k;i++){
+		sum-=diff[n-i];
+	}
+	cout<<sum;
+   }
