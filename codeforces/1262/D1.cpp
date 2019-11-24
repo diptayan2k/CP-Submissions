c4ba30db0e02d45ec53d8ac3721b56a9443f4c3a
Diptayan Biswas <diptayan2k@gmail.com>
2019-11-24 20:04:00 +0530

    Add solution for problem `D1 - Optimal Subsequences (Easy Version)` Link: https://codeforces.com/contest/1262/problem/D1 Tags: data structures, greedy, *1600 Ref: https://codeforces.com/contest/1262/submission/65667637

---
+++ codeforces/1262/D1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define f(i,a,b) for(ll i=a;i<=b;i++)
+#define g(i,a,b) for(ll i=a;i>=b;i--)
+#define F first
+#define vv vector
+#define S second
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define ds set <ll, greater<ll> >
+using namespace std;
+
+bool sortinrev(const pair<ll,ll> &a, const pair<ll,ll> &b)
+{
+       if(a.first == b.first)
+       {
+            return (a.second < b.second);
+       }
+
+       else
+       return (a.first > b.first);
+}
+
+
+int main()
+{
+
+
+ll n;
+cin>>n;
+ll a[n];
+
+vector< pair<ll,ll> > vvv;
+f(i,0,n-1) {
+cin>>a[i];
+}
+f(i,0,n-1)
+vvv.pb({a[i],i});
+sort(vvv.begin(),vvv.end(),sortinrev);
+
+
+
+ll t;
+cin>>t;
+
+while(t--)
+{
+    ll k,p;
+    cin>>k>>p;
+    vector< pair<ll,ll> > v;
+
+
+    f(i,0,k-1)
+    {
+        v.pb({vvv[i].S,vvv[i].F});
+    }
+
+    sort(v.begin(),v.end());
+
+    cout<<v[p-1].S<<endl;
+
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
