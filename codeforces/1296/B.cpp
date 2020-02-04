0f5010974e6fc681439e2c01e380fd25a42762ab
Diptayan Biswas <diptayan2k@gmail.com>
2020-02-04 20:25:00 +0530

    Add solution for problem `B - Food Buying` Link: https://codeforces.com/contest/1296/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1296/submission/70256039

---
+++ codeforces/1296/B.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define f(i,a,b) for(ll i=a;i<=b;i++)
+#define g(i,a,b) for(ll i=a;i>=b;i--)
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+void solve()
+{
+  ll n;
+  cin>>n;
+  ll x=n;
+  ll an=0;
+  while(x>=10)
+  {
+      ll p=x/10;
+      an+=p*10;
+      x=x%10+p;
+
+
+  }
+  an+=x;
+  cout<<an<<endl;
+
+}
+
+int main()
+{
+   ll t;
+   cin>>t;
+   while(t--)
+   {
+       solve();
+   }
+
+}
