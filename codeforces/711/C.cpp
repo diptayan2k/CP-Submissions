a60ee6efdd42e2478f9f6f38f4bc41d1c3bd66e5
Diptayan Biswas <diptayan2k@gmail.com>
2020-03-01 16:07:00 +0530

    Add solution for problem `C - Coloring Trees` Link: https://codeforces.com/contest/711/problem/C Tags: dp, *1700 Ref: https://codeforces.com/contest/711/submission/72154767

--- codeforces/711/C.cpp
+++ codeforces/711/C.cpp
@@ -1,99 +1,70 @@
-#include <iostream>
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#define ll long long int
-#define f(i,a,b) for(ll i=a;i<=b;i++)
-#define g(i,a,b) for(ll i=a;i>=b;i--)
-#define init(arr,val) memset(arr,val,sizeof(arr))
-#define F first
-#define S second
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
 #define mp make_pair
 #define pb push_back
-#define mh make_heap
-#define ph push_heap
-#define pq priority_queue
+#define F first
+#define S second
+const int N=100005;
+#define M 1000000007
+#define double long double
+#define BINF 1000000000000001
+#define init(arr,val) memset(arr,val,sizeof(arr))
+#define MAXN 5000004
+#define deb(x) cout << #x << " " << x << "\n";
+const int LG = 22;
 
-using namespace std;
-using namespace __gnu_pbds;
-typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
-ll mod=1000000000000000+7;
-ll p[105][105];
-ll dp[105][105][105];
-ll n,m,k;
-ll a[105];
-ll foo(ll i, ll bt, ll prev)
-{
-    if(i==n+1)
-    {
-        if(bt==k+1) return 0;
-        return mod;
+int n, m, k;
+int a[105];
+int dp[101][105][101], p[105][105];
 
+int foo(int ind, int gr, int prev){
+    if(ind == n + 1){  
+        if(gr == k + 1)
+            return 0;
+        return BINF;
     }
+    if(dp[ind][gr][prev] != -1)
+        return dp[ind][gr][prev];
 
-    if(dp[i][bt][prev]!=-1) return dp[i][bt][prev];
-    ll res=mod;
-
-    if(a[i]!=0)
-    {
-        res=min(res,foo(i+1,bt+(prev!=a[i]),a[i]));
-    }
-    else
-    {
-        f(j,1,m)
-        {
-            res=min(res,p[i][j]+foo(i+1,bt+(prev!=j),j));
+    int res = BINF;
+    if(a[ind] != 0){
+        res = min(res, foo(ind + 1, gr + (prev != a[ind]), a[ind]));
+    }else{
+        for(int i = 1; i <= m; i++){
+            res = min(res, p[ind][i] + foo(ind + 1, gr + (prev != i), i));
         }
     }
-
-    return dp[i][bt][prev]=res;
-
+    return dp[ind][gr][prev] = res;
 }
 
 
-void solve(ll d)
-{
+#undef int 
+int main() {
+#define int long long int
+ios_base::sync_with_stdio(false); 
+cin.tie(0); 
+cout.tie(0);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("optput.txt", "w", stdout); 
+#endif
 
-    cin>>n>>m>>k;
-    f(i,1,n) cin>>a[i];
+    init(dp, -1);
 
-    f(i,1,n)
-    {
-        f(j,1,m) cin>>p[i][j];
-    }
+    cin >> n >> m >> k;
+    for(int i = 1; i <= n; i++)
+        cin >> a[i];
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+            cin >> p[i][j];
 
-    f(i,0,104) f(j,0,104) f(k,0,104) dp[i][j][k]=-1;
-
-    ll ans=foo(1,1,-1);
-    if(ans>=mod)
-    {
-        cout<<-1<<endl;
-    }
-    else
-    {
-        cout<<ans<<endl;
-    }
-
+    int ans = foo(1, 1, 102);
+    if(ans == BINF)
+        ans = -1;
+    cout << ans << endl;
+   
+return 0;  
 }
-
-
-
-
-
-int main()
-{
-
-
-    ll t=1;
-
-    //cin>>t;
-    f(i,1,t)
-    {
-        solve(i);
-    }
-
-}
-
-
-
