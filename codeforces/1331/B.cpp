c4afb3a2ddcd85793e0aa053ecc7b00c3b649f14
Diptayan Biswas <diptayan2k@gmail.com>
2020-04-01 20:25:00 +0530

    Add solution for problem `B - Limericks` Link: https://codeforces.com/contest/1331/problem/B Tags: math, number theory Ref: https://codeforces.com/contest/1331/submission/75114783

---
+++ codeforces/1331/B.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define f(i,a,b) for(ll i=a;i<=b;i++)
+#define g(i,a,b) for(ll i=a;i>=b;i--)
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcount(x)
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ll mod=1e9+7;
+
+
+
+
+
+
+void solve(ll t)
+{
+    vector<ll> v;
+    ll n;
+    cin>>n;
+    while(n%2==0)
+    {
+        n/=2;
+        v.pb(2);
+    }
+    for(ll i=3;i<=sqrt(n);i+=2)
+    {
+        while(n%i==0)
+        {
+            n/=i;
+            v.pb(i);
+        }
+    }
+    if(n>2)
+    {
+        v.pb(n);
+    }
+    sort(v.begin(),v.end());
+    for(auto i:v)
+        cout<<i;
+
+
+
+
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t=1;
+    //cin>>t;
+
+    f(i,1,t)
+    {
+        solve(i);
+    }
+
+}
+
