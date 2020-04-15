03b7475b6b28e66869efe6cda45f355e5a823c62
Diptayan Biswas <diptayan2k@gmail.com>
2020-04-15 20:10:00 +0530

    Add solution for problem `A - Ichihime and Triangle` Link: https://codeforces.com/contest/1337/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1337/submission/76812438

---
+++ codeforces/1337/A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcount(x)
+using namespace std;
+
+
+void solve(ll t)
+{
+     ll a,b,c,d;
+     cin>>a>>b>>c>>d;
+
+    cout<<b<<" "<<c<<" "<<c<<endl;
+
+
+}
+
+
+int main()
+{
+    ll t=1;
+    scanf("%lld",&t);
+
+    f(i,1,t)
+    {
+        solve(i);
+    }
+
+}
