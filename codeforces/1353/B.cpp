c84f0219453e661895a47410928235b3b09bf4b8
Diptayan Biswas <diptayan2k@gmail.com>
2020-05-15 19:51:00 +0530

    Add solution for problem `B - Two Arrays And Swaps` Link: https://codeforces.com/contest/1353/problem/B Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1353/submission/80234228

---
+++ codeforces/1353/B.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Case "<<x<<": "
+using namespace std;
+ll mod = 1e9 + 7;
+ll INF = 1000000000000000000;
+
+void solve(int t)
+{
+	ll n, k;
+	cin >> n >> k;
+	ll a[n];
+	ll b[n];
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = 0; i < n; i++) cin >> b[i];
+	sort(a, a + n, greater<int>());
+	sort(b, b + n, greater<int>());
+	ll p = n - 1;
+	ll q = 0;
+	while (p >= 0 and q<n and k>0)
+	{
+		if (a[p] < b[q])
+		{
+			swap(a[p], b[q]);
+			p--;
+			q++;
+			k--;
+		}
+		else p--;
+	}
+	//for (int i = 0; i < n; i++) cout << a[i] << " ";
+	//cout << endl;
+	ll ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		ans += a[i];
+	}
+	cout << ans << endl;
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
