a3d1e9d60087345290b3ab7778194c40403b4906
Diptayan Biswas <diptayan2k@gmail.com>
2020-05-15 12:32:00 +0530

    Add solution for problem `A - Most Unstable Array` Link: https://codeforces.com/contest/1353/problem/A Tags: constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1353/submission/80199111

---
+++ codeforces/1353/A.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Case "<<x<<": "
+using namespace std;
+ll mod = 1e9 + 7;
+ll INF = 1000000000000000000;
+
+void solve(int t)
+{
+	ll n, m;
+	cin >> n >> m;
+	if (n == 1)
+	{
+		cout << 0 << endl;
+	}
+	else if (n == 2)
+	{
+		cout << m << endl;
+	}
+	else
+	{
+		cout << 2 * m << endl;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
