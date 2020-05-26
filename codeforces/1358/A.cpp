63df58754f4d92df693689e477ea1d6c94f3bed5
Diptayan Biswas <diptayan2k@gmail.com>
2020-05-26 20:22:00 +0530

    Add solution for problem `A - Park Lighting` Link: https://codeforces.com/contest/1358/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1358/submission/81496686

---
+++ codeforces/1358/A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Case "<<x<<": "
+using namespace std;
+const ll mod = 1e9 + 7;
+const ll INF = 1e18;
+const int N = 21;
+
+void solve(int t)
+{
+	ll n, m;
+	cin >> n >> m;
+	ll ans = INF;
+	ll d = n / 2;
+	ans = min(ans, d * m + (n % 2) * (m / 2 + m % 2));
+	swap(m, n);
+	d = n / 2;
+	ans = min(ans, d * m + (n % 2) * (m / 2 + m % 2));
+	cout << ans << endl;
+
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
