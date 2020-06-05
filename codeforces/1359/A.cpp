3582ab3366a9f8c30d62252c76acd36640312e79
Diptayan Biswas <diptayan2k@gmail.com>
2020-06-05 20:04:00 +0530

    Add solution for problem `A - Berland Poker` Link: https://codeforces.com/contest/1359/problem/A Tags: brute force, greedy, math, *1000 Ref: https://codeforces.com/contest/1359/submission/82639174

---
+++ codeforces/1359/A.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Scenario #"<<x<<": "
+using namespace std;
+const ll mod = 1000000007;
+const ll INF = 1e18;
+const int N = 21;
+
+
+void solve(int t)
+{
+	ll n, k, m;
+	cin >> n >> m >> k;
+
+	ll d = min(n / k, m);
+	ll p = m - d;
+	ll r = p / (k - 1);
+
+	ll q = max(r, r + (p % (k - 1) != 0));
+	cout << (d - q) << endl;
+
+
+}
+
+int main()
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
