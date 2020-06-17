cd622b5a2ee22fc088182d1699fb6f682427ec5a
Diptayan Biswas <diptayan2k@gmail.com>
2020-06-17 22:37:00 +0530

    Add solution for problem `C - Hexadecimal's Numbers` Link: https://codeforces.com/contest/9/problem/C Tags: brute force, implementation, math, *1200 Ref: https://codeforces.com/contest/9/submission/84096861

---
+++ codeforces/9/C.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Scenario #"<<x<<": "
+using namespace std;
+const ll mod = 1000000007;
+const ll INF = 1e18;
+const int N = 21;
+ll n;
+
+ll foo(ll num)
+{
+	ll res = 0;
+	if (num * 10 + 1 <= n  and num * 10 + 1 > 0)
+	{
+		res += (1 + foo(num * 10 + 1));
+	}
+	if (num * 10 <= n and num * 10 > 0)
+	{
+		res += (1 + foo(num * 10));
+	}
+	return res;
+}
+
+void solve(int t)
+{
+	cin >> n;
+
+	cout << foo(0) << endl;
+}
+
+int main()
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	//cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
