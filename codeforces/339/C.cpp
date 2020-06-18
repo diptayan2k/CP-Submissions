f33bf03dfa44d9f3cf964638776b0cb3e6b39ed8
Diptayan Biswas <diptayan2k@gmail.com>
2020-06-18 11:39:00 +0530

    Add solution for problem `C - Xenia and Weights` Link: https://codeforces.com/contest/339/problem/C Tags: constructive algorithms, dfs and similar, dp, graphs, greedy, shortest paths, *1700 Ref: https://codeforces.com/contest/339/submission/84129569

---
+++ codeforces/339/C.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Scenario #"<<x<<": "
+using namespace std;
+const ll mod = 1000000007;
+const ll INF = 1e18;
+const int N = 21;
+ll m;
+string s;
+ll a[1001];
+ll final[1001];
+ll ok = 0;
+ll dp[1001][11][11];
+
+ll foo(ll n, ll diff, ll prev)
+{
+	if (n == m)
+	{
+		if (ok == 0)
+		{
+			for (int i = 0; i < m; i++) final[i] = a[i];
+			ok = 1;
+		}
+		return true;
+	}
+
+	if (dp[n][diff][prev] != -1) return dp[n][diff][prev];
+
+	bool res = false;
+
+	for (int i = diff + 1; i <= 10; i++)
+	{
+		if (s[i - 1] == '1' and i != prev)
+		{
+			a[n] = i;
+			res |= foo(n + 1, i - diff, i);
+		}
+	}
+
+	return dp[n][diff][prev] = res;
+
+
+}
+
+void solve(int t)
+{
+	cin >> s;
+	cin >> m;
+	memset(dp, -1, sizeof(dp));
+
+	if (foo(0, 0, 0))
+	{
+		cout << "YES" << endl;
+		for (int i = 0; i < m; i++) cout << final[i] << " ";
+		cout << endl;
+		return;
+
+	}
+	cout << "NO" << endl;
+
+}
+
+int main()
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	//cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
