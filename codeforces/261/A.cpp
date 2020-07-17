b3011ecbcaaec31b23b83ea599b91fa4df5d8248
Diptayan Biswas <diptayan2k@gmail.com>
2020-07-17 12:20:00 +0530

    Add solution for problem `A - Maxim and Discounts` Link: https://codeforces.com/contest/261/problem/A Tags: greedy, sortings, *1400 Ref: https://codeforces.com/contest/261/submission/87045848

---
+++ codeforces/261/A.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define mp make_pair
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Scenario #"<<x<<": "
+#define endl "\n"
+using namespace std;
+const ll mod = 1000000007;
+const ll INF = 1e18;
+const int N = 20;
+
+
+void solve(int t)
+{
+
+	ll m;
+	cin >> m;
+	ll a[m];
+	for (int i = 0; i < m; i++) cin >> a[i];
+	sort(a, a + m, greater<ll>());
+	ll n;
+	cin >> n;
+	ll b[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> b[i];
+	}
+	sort(b, b + n, greater<ll>());
+
+	ll sum = 0;
+
+	ll k = 0;
+
+
+	while (n - k  >= a[m - 1] and k < n)
+	{
+		for (int j = 0; j < a[m - 1]; j++)
+		{
+			sum += b[k];
+			k++;
+		}
+
+		if (k < n)
+		{
+			k++;
+		}
+		if (k < n)
+		{
+			k++;
+		}
+		//cout << k << endl;
+	}
+
+
+
+
+	for (int i = k; i < n; i++)
+	{
+		sum += b[i];
+	}
+	cout << sum << endl;
+
+
+}
+
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	//cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
