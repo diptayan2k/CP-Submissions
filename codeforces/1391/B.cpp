1f332e642c3269f8b15a3faecc652083b9efa492
Diptayan Biswas <diptayan2k@gmail.com>
2020-08-09 20:14:00 +0530

    Add solution for problem `B - Fix You` Link: https://codeforces.com/contest/1391/problem/B Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/1391/submission/89417723

---
+++ codeforces/1391/B.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Scenario #"<<x<<": "
+#define endl "\n"
+using namespace std;
+const ll mod = 1000000007;
+const ll INF = 1e18;
+const int N = 18;
+
+
+
+
+void solve(int t)
+{
+	ll n, m;
+	cin >> n >> m;
+
+	string s[n];
+	for (int i = 0; i < n; i++) cin >> s[i];
+
+	ll cnt = 0;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (s[i][m - 1] == 'R')
+		{
+			cnt++;
+		}
+	}
+
+	for (int i = 0; i < m - 1; i++)
+	{
+		if (s[n - 1][i] == 'D') cnt++;
+	}
+
+	cout << cnt << endl;
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
