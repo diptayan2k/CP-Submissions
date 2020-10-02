d93b78fa5a2ab810d951cb11a250cf815ee87147
Diptayan Biswas <diptayan2k@gmail.com>
2020-10-02 19:46:00 +0530

    Add solution for problem `B - Two Arrays` Link: https://codeforces.com/contest/1417/problem/B Tags: greedy, math, sortings, *1100 Ref: https://codeforces.com/contest/1417/submission/94496922

---
+++ codeforces/1417/B.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Scenario #"<<x<<": "
+#define endl "\n"
+using namespace std;
+const ll mod = 998244353;
+const ll INF = LLONG_MAX;
+const ll NEGINF = LLONG_MIN;
+const int N = 18;
+const ll MAXN = 1000001;
+
+
+
+void solve(int t)
+{
+	ll n, k;
+	cin >> n >> k;
+	ll a[n];
+	map<ll, ll> mp;
+
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	for (int i = 0; i < n; i++) mp[a[i]] = -1;
+	ll ans[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		if (mp[k - a[i]] == -1)
+		{
+			mp[a[i]] = 0;
+			ans[i] = 0;
+		}
+		else
+		{
+
+			ll x  = (mp[k - a[i]] ^ 1);
+			ans[i] = x;
+			//cout << x << " " << mp[a[i]] << endl;
+			mp[a[i]] = x;
+		}
+
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << ans[i] << " ";
+	}
+
+	cout << endl;
+
+
+
+
+
+
+}
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+	int t = 1;
+	cin >> t;
+
+
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
