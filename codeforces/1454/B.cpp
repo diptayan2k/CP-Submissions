26869a34866087c33e5dad98c8684782893bd4cf
Diptayan Biswas <diptayan2k@gmail.com>
2020-11-27 20:13:00 +0530

    Add solution for problem `B - Unique Bid Auction` Link: https://codeforces.com/contest/1454/problem/B Tags: implementation, *800 Ref: https://codeforces.com/contest/1454/submission/99725728

---
+++ codeforces/1454/B.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define f(i,a,b) for(int i=a;i<=b;i++)
+#define g(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mh make_heap
+#define ph push_heap
+#define pq priority_queue
+#define bits(x) __builtin_popcountll(x)
+#define op(x) cout<<"Case #"<<x<<": "
+#define op1(x) cout<<"Scenario #"<<x<<": "
+#define endl "\n"
+using namespace std;
+const ll mod1 = 1000000007;
+const ll mod = 998244353;
+const ll INF = 1000000000000000000;//LLONG_MAX;
+const ll NEGINF = LLONG_MIN;
+const int N = 18;
+const ll MAXN = 1000001;
+
+
+
+void solve(int t)
+{
+	ll n;
+	cin >> n;
+	ll a[n];
+
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	map<ll, ll> mp, mp1;
+
+	ll k = 0;
+	for (auto i : a) mp[i]++, mp1[i] = ++k;
+
+	for (auto i : mp)
+	{
+		if (i.S == 1)
+		{
+			cout << mp1[i.F] << endl;
+			return;
+		}
+	}
+
+	cout << -1 << endl;
+
+
+
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+
+	int t = 1;
+	cin >> t;
+
+
+
+	for (int i = 1; i <= t; i++)
+	{
+		solve(i);
+
+	}
+}
